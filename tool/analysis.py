63a7a4b3e67575a58a734be15da7fbaf12ac9a83
Siddhartha Banerjee <sidban@uwalumni.com>
2020-06-12 15:16:15 -0700

    Add method for PDF
    Probability Density of any number of dimension is calculated in method.

--- tool/analysis.py
+++ tool/analysis.py
@@ -23,3 +23,40 @@ class ProbabilityDensity:
             assert item > 0, 'Input numbers greater than zero.'
         self._number_of_bins = args
         self._number_of_dims = args.__len__()
+
+    def pdf(
+            self,
+            data: list = None,
+            mass: list = None,
+    ) -> np.ndarray:
+        """Return Probability Density."""
+        data = np.asarray(data)
+        assert data.shape[0] == self._number_of_dims, \
+            'Dimension is not matched.'
+        if mass is None:
+            mass = np.ones_like(data[0])
+        else:
+            mass = np.asarray(mass)
+        x_bins = []
+        total_mass = mass.sum()
+        idx = np.empty_like(data)
+        PDFo = np.zeros(shape=self._number_of_bins)
+        for idim, idata in enumerate(data):
+            x_bins.append(
+                np.linspace(
+                    start=idata.min(),
+                    stop=idata.max(),
+                    num=self._number_of_bins[idim]
+                )
+            )
+            idx[idim] = (
+                (
+                        idata - idata.min()
+                ) / (
+                        np.ptp(idata)
+                ) * (self._number_of_bins[idim] - 1)
+            )
+        for inum, index in enumerate(idx.T):
+            PDFo[tuple(index.astype(int))] = mass[inum]
+
+        return PDFo / total_mass
