6d5bd5149e340775dc8a9d96e0d6007f2905fbd7
Siddhartha Banerjee <sidban@uwalumni.com>
2020-06-12 22:07:59 -0700

    Adding docstrings
    Docstrings added to the analysis tool (`tool.analysis`) and `ProbabilityDensity` class.

--- tool/analysis.py
+++ tool/analysis.py
@@ -1,5 +1,27 @@
-"""
-Modules for statistical analysis.
+"""Statistical analysis module.
+
+This module provides tools for statistical analysis.
+
+NAME
+    analysis
+CLASSES
+    ProbabilityDensity
+
+Typical Usage
+==============
+::
+
+    from tool.analysis import ProbabilityDensity as Pd
+    import matplotlib.pyplot as plt
+    pd = Pd(5, 10)
+    x, p = pd.pdf(
+        data=[[3, 5, 6, 7, 6, 4], [4, 3, 9, 6, 7, 6]],
+        mass=[1, 2, 1, 3, 2, 1],
+    )
+    fig = plt.contourf(x[0], x[1], p)
+
+x is the grid of input values
+p is the mass weighted probability density of x
 
 @author: siddhartha.banerjee [sidban@uwalumni.com]
 """
@@ -8,8 +30,15 @@ import numpy as np
 
 
 class ProbabilityDensity:
-    """
-    Class to get probability density.
+    """Class to get probability calculation.
+
+    An object to calculate probability density of a continuous variable in
+    arbitrary number of dimensions (>= 1).
+
+    Typical Usage - How to Instantiate an object
+    ============
+    ::
+        pd = ProbabilityDensity(number_of_bins_x0, number_of_bins_x1, ...)
     """
 
     def __init__(
@@ -28,8 +57,25 @@ class ProbabilityDensity:
             self,
             data: list = None,
             mass: list = None,
-    ) -> np.ndarray:
-        """Return Probability Density."""
+    ) -> tuple:
+        """Return Probability Density.
+
+        Typical Usage
+        =============
+        ::
+            x, p = pd.pdf(
+                data=[[3, 5, 6, 7, 6, 4], [4, 3, 9, 6, 7, 6]],
+                mass=[1, 2, 1, 3, 2, 1],
+            )
+
+        :parameter
+        data: is a list of [x[i]], where x[i] is a list of data on x_i
+        mass: (optional) is the weighted mass of the data
+
+        :returns
+        x: numpy.meshgrid containing the mapped x data from all bins
+        p: probability density of x in all given dimensions.
+        """
         data = np.asarray(data)
         assert data.shape[0] == self._number_of_dims, \
             'Dimension is not matched.'
@@ -40,7 +86,7 @@ class ProbabilityDensity:
         x_bins = []
         total_mass = mass.sum()
         idx = np.empty_like(data)
-        PDFo = np.zeros(shape=self._number_of_bins)
+        pdf_o = np.zeros(shape=self._number_of_bins)
         for idim, idata in enumerate(data):
             x_bins.append(
                 np.linspace(
@@ -57,6 +103,6 @@ class ProbabilityDensity:
                 ) * (self._number_of_bins[idim] - 1)
             )
         for inum, index in enumerate(idx.T):
-            PDFo[tuple(index.astype(int))] = mass[inum]
+            pdf_o[tuple(index.astype(int))] = mass[inum]
 
-        return PDFo / total_mass
+        return np.meshgrid(*tuple(x_bins)), pdf_o.T / total_mass
